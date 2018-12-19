a02cab6c6859bdee33c054e09999330964129faa
kedards25 <kedards25@gmail.com>
2018-12-19 07:45:32 +0530

    GUI Coding and Table list added

---
+++ Glorious/src/glorious/Glorious.java
@@ -0,0 +1,29 @@
+package glorious;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import Shared.ConnectionManager;
+
+/**
+ *
+ * @author vinay sawant
+ */
+public class Glorious {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("From main");
+       // ConnectionManager cm=new ConnectionManager();
+        //admin_login obj=new admin_login();
+       // System.out.println(cm.getConnection());
+    }
+    
+}
